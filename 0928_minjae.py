efdfb1e99a368c9621b101c62c8e2faa394d7db6
aldrn3333 <80663462+aldrn3333@users.noreply.github.com>
2021-09-28 11:40:05 +0900

    Create 0928_minjae.py

---
+++ 0928_minjae.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = 0
+    for i in str(n):
+        answer += int(i)
+    return answer
+    
+print(solution(5923)) #19
+print(solution(200)) #2
+print(solution(1234567890)) #45
+print(solution(2364759387)) #54
