eecbf29eeae3fe7c84218f423bea21c6b67deb28
ozloz8 <84396904+ozloz8@users.noreply.github.com>
2021-09-28 11:42:05 +0900

    Create minji - lab1_template.py

---
+++ 0928_assignment/minji - lab1_template.py
@@ -0,0 +1,10 @@
+def solution(num):
+    n = list(map(int, str(num)))
+    answer = sum(n)
+    return answer
+
+
+print(solution(5923)) #19
+print(solution(200)) # 2
+print(solution(1234567890)) #45
+print(solution(2364759387)) #54
