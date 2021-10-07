990050b334e0abac145e81f4bca4b2abdabc8135
minchan481 <55022230+minchan481@users.noreply.github.com>
2021-10-07 11:47:52 +0900

    Update and rename a to minchan-fibo.py

---
+++ 10.07/minchan-fibo.py
@@ -0,0 +1,32 @@
+import time
+import random
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+def iterfibo(n):
+    if n <= 1:
+        return n
+    num1 = 0
+    num2 = 1
+    ans = 0
+    for i in range(n - 1):
+        ans = num1 + num2
+        num1 = num2
+        num2 = ans
+    return ans
+
+while True:
+    nbr = int(input("Enter a number: "))
+    if nbr == -1:
+        break
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print("IterFibo(%d) = %d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
