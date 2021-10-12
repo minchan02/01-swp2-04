6dff5c587e2376bd1dcf48e7736dded656c0132e
aldrn3333 <80663462+aldrn3333@users.noreply.github.com>
2021-10-12 10:21:06 +0900

    Update assignment3_20212970.py

--- 0930/assignment3_20212970.py
+++ 0930/assignment3_20212970.py
@@ -31,39 +31,48 @@ def doScoreDB(scdb):
     while(True):
         inputstr = (input("Score DB > "))
         
-        if inputstr == "": continue
-        parse = inputstr.split(" ")
+        try:
+            if inputstr == "": continue
+            parse = inputstr.split(" ")
         
-        if parse[0] == 'add':
-            record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
-            scdb += [record]
+            if parse[0] == 'add':
+                record = {'Name':parse[1], 'Age':parse[2], 'Score':parse[3]}
+                scdb += [record]
             
-        elif parse[0] == 'del':
-            for p in scdb:
-                if p['Name'] == parse[1]:
-                    scdb.remove(p)    
-                    break
+           
+            elif parse[0]=='del': #Del all same name 
+	            i=len(scdb)-1
+	            whilei>=0:
+		            if scdb[i]["Name"]==parse[1]:
+			           delscdb[i]
+				       i-=1
                     
-        elif parse[0] == 'show':
-            sortKey ='Name' if len(parse) == 1 else parse[1]
-            showScoreDB(scdb, sortKey)
-            
-        elif parse[0] == 'find':
-            name = input("Name:")
-            for i in scdb:
-                if i["Name"] == name:
-                    print("Name:", name)
+            elif parse[0] == 'show':
+                sortKey ='Name' if len(parse) == 1 else parse[1]
+                showScoreDB(scdb, sortKey)
+                
+            elif parse[0]=="find": #find given name
+	            for i in scdb:
+		            if parse[1] == i["Name"]:
+			            for v in sorted(i):
+				            print(v + "=" + str(i[v]), end=' ')
+				            print()
                     
-        elif parse[0] == 'inc':
-            for k in scdb:
-                if k['Name'] == parse[1]:
-                    k['Score'] += int(parse[2])
-                    
-        elif parse[0] == 'quit':
-            break
-            
-        else:
-            print("Invalid command: " + parse[0])
+            elif parse[0]=="inc": #amount score given name
+	            for i in scdb:
+		            if parse[1] == i["Name"]:
+			            i["Score"] += int(parse[2])
+		            else:
+			            print("Invalid command: "+parse[0])
+
+            elif parse[0] == 'quit':
+                break
+
+            else:
+                print("Invalid command: " + parse[0])
+                
+        except:
+            print("error")
 
 
 def showScoreDB(scdb, keyname):
