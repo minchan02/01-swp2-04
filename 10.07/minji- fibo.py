ac5f2739d5894c059c50d3c1f46c22f3d7d24c61
minwoogood12 <84243851+minwoogood12@users.noreply.github.com>
2021-10-18 23:37:55 +0900

    Update minji- fibo.py
    f1 s2 t3 ==> f1 s2 변수 2개만 사용하였습니다.

--- 10.07/minji- fibo.py
+++ 10.07/minji- fibo.py
@@ -6,13 +6,19 @@ def fibo(n):
         return n
     return fibo(n - 1) + fibo(n - 2)
 
+
 def iterfibo(n):
-    if n <= 1 :
+    if n <= 1:
         return n
-    a, b = 0, 1
-    for i in range(n-1):
-        a, b = b, (a+b)
-    return b
+
+    f1 = 0
+    s2 = 1
+
+    for i in range(1, n):
+        f1, s2 = s2, f1 + s2
+
+    return s2
+
 
 while True:
     nbr = int(input("Enter a number: "))
@@ -20,10 +26,9 @@ while True:
         break
     ts = time.time()
     fibonumber = iterfibo(nbr)
-    ts = time.time() -ts
-    print("IterFibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
+    ts = time.time() - ts
+    print("IterFibo(%d) = %d, time %.6f" %( nbr, fibonumber, ts))
     ts = time.time()
     fibonumber = fibo(nbr)
     ts = time.time() - ts
     print("Fibo(%d)=%d, time %.6f" %(nbr, fibonumber, ts))
-
