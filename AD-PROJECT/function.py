5586f80375c6e0cb0271457a9f72833822e51825
minchan481 <55022230+minchan481@users.noreply.github.com>
2021-11-30 20:51:32 +0900

    Update decyption
    add decryption function

--- AD-PROJECT/function.py
+++ AD-PROJECT/function.py
@@ -8,3 +8,13 @@ class LockString:
             elif m[i].islower():
                 m[i] = chr((ord(m[i]) - ord('a') + n) % 26 + ord('a'))
         return ''.join(m)
+    
+    # 복호화 함수
+    def decryption(self, word, n):
+        m = list(word)
+        for i in range(len(m)):
+            if m[i].isupper():
+                m[i] = chr((ord(m[i]) - ord('A') - n) % 26 + ord('A'))
+            elif m[i].islower():
+                m[i] = chr((ord(m[i]) - ord('a') - n) % 26 + ord('a'))
+        return ''.join(m)
