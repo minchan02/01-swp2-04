f0d69d8c3a79b539cb30c306a202dd0ea515c40e
kimseojeong <kimsj8912@kookmin.ac.kr>
2021-11-30 20:41:43 +0900

    암호화 함수
    암호화 함수 encryption 추가

---
+++ AD-PROJECT/function.py
@@ -0,0 +1,10 @@
+class LockString:
+    # 암호화 함수
+    def encryption(self, word, n):
+        m = list(word)
+        for i in range(len(m)):
+            if m[i].isupper():
+                m[i] = chr((ord(m[i]) - ord('A') + n) % 26 + ord('A'))
+            elif m[i].islower():
+                m[i] = chr((ord(m[i]) - ord('a') + n) % 26 + ord('a'))
+        return ''.join(m)
