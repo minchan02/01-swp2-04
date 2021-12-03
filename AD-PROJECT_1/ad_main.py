174d45d4817218b63d24daac2e5b02504651f11c
ozloz8 <84396904+ozloz8@users.noreply.github.com>
2021-12-03 20:03:58 +0900

    Add files via upload

---
+++ AD-PROJECT_1/ad_main.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import QtGui
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        start_button = QPushButton('Start')  # 시작버튼
+        start_button.resize(100, 500)
+        #start_button.clicked.connect(inGame) << 버튼 연결을 못하겠습ㄴㅣ다 ........
+
+        title_label = QLabel('카이사르 암호 게임')  # 제목 라벨
+        font = title_label.font()
+        font.setBold(True)
+        title_label.setFont(QtGui.QFont('Noto Sans KR',20))
+        title_label.setAlignment(Qt.AlignHCenter)
+
+        hbox1 = QHBoxLayout()
+        hbox1.addStretch(1)
+        hbox1.addWidget(title_label)
+        hbox1.addStretch(1)
+
+        hbox2 = QHBoxLayout()
+        hbox2.addStretch(1)
+        hbox2.addWidget(start_button)
+        hbox2.addStretch(1)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox1)
+        vbox.addStretch(2)
+        vbox.addLayout(hbox2)
+        vbox.addStretch(1)
+
+        self.setLayout(vbox)
+
+        self.setWindowTitle("카이사르 암호 게임")
+        self.setGeometry(600,200,400,400)
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWidget()
+    sys.exit(app.exec_())
